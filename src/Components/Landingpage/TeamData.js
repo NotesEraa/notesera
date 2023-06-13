e20d1768cc91aede696691804207b1a915fe81e1
guptaarth87 <guptaarth87@gmail.com>
2023-06-13 12:34:18 +0530

    initial commit

---
+++ src/Components/Landingpage/TeamData.js
@@ -0,0 +1,112 @@
+import annirudh from '../../Assets/Teams/annirudhh.jpg';
+import arth from '../../Assets/Teams/Arth.jpg';
+import ayush from '../../Assets/Teams/Ayush saxena.jpg';
+import sakshi from '../../Assets/Teams/sakshi.jpg';
+import jerin from '../../Assets/Teams/jerin.jpg';
+import daaniyal from '../../Assets/Teams/daaniyal.jpg';
+import aradhya from '../../Assets/Teams/aradhya.jpg';
+
+const  teamList = [
+    {    'id':0,
+          'name':"sakshi",
+         'img_url':sakshi,
+         'instagram_url':"https://www.codester",
+         'github_url':"https://www.codes",
+         'linkedin_url':"https://www.codester"
+    },
+    {    'id':1,
+         'name':"Ayush saxena",
+         'img_url':ayush,
+         'instagram_url':"https://www.codester",
+         'github_url':"https://www.codes",
+         'linkedin_url':"https://www.codester"
+    },
+    {    'id':2,
+       'name':"arth",
+    'img_url':arth,
+    'instagram_url':"https://www.codester",
+    'github_url':"https://www.codes",
+    'linkedin_url':"https://www.codester"
+    },
+    {    'id':3,
+          'name':"annirudh",
+         'img_url':annirudh,
+         'instagram_url':"https://www.codester",
+         'github_url':"https://www.codes",
+         'linkedin_url':"https://www.codester"
+    },
+    {    'id':4,
+        'name':"aradhya",
+         'img_url':aradhya,
+         'instagram_url':"https://www.codester",
+         'github_url':"https://www.codes",
+         'linkedin_url':"https://www.codester"
+    },
+    {    'id':5,
+          'name':"daaniyal",
+         'img_url':daaniyal,
+         'instagram_url':"https://www.codester",
+         'github_url':"https://www.codes",
+         'linkedin_url':"https://www.codester"
+    },
+    {    'id':6,
+    'name':"jerin",
+         'img_url':jerin,
+         'instagram_url':"https://www.codester",
+         'github_url':"https://www.codes",
+         'linkedin_url':"https://www.codester"
+    },
+    {    'id':0,
+    'name':"sakshi",
+   'img_url':sakshi,
+   'instagram_url':"https://www.codester",
+   'github_url':"https://www.codes",
+   'linkedin_url':"https://www.codester"
+},
+{    'id':1,
+   'name':"Ayush saxena",
+   'img_url':ayush,
+   'instagram_url':"https://www.codester",
+   'github_url':"https://www.codes",
+   'linkedin_url':"https://www.codester"
+},
+{    'id':2,
+ 'name':"arth",
+'img_url':arth,
+'instagram_url':"https://www.codester",
+'github_url':"https://www.codes",
+'linkedin_url':"https://www.codester"
+},
+{    'id':3,
+    'name':"annirudh",
+   'img_url':annirudh,
+   'instagram_url':"https://www.codester",
+   'github_url':"https://www.codes",
+   'linkedin_url':"https://www.codester"
+},
+{    'id':4,
+  'name':"aradhya",
+   'img_url':aradhya,
+   'instagram_url':"https://www.codester",
+   'github_url':"https://www.codes",
+   'linkedin_url':"https://www.codester"
+},
+{    'id':5,
+    'name':"daaniyal",
+   'img_url':daaniyal,
+   'instagram_url':"https://www.codester",
+   'github_url':"https://www.codes",
+   'linkedin_url':"https://www.codester"
+},
+{    'id':6,
+'name':"jerin",
+   'img_url':jerin,
+   'instagram_url':"https://www.codester",
+   'github_url':"https://www.codes",
+   'linkedin_url':"https://www.codester"
+},
+   
+   ];
+
+
+export default teamList;
